beccc2e27496d255cf8c2de7e4aeb496fdadc243
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-08-29 23:28:19 -0700

    add my solution

---
+++ small_problems/02_easy_2/05_combine_two_lists.py
@@ -0,0 +1,17 @@
+# Combine Two Lists
+# Write a function that combines two lists passed as arguments and returns a new list that contains all elements from both list arguments, with each element taken in alternation.
+
+# You may assume that both input lists are non-empty, and that they have the same number of elements.
+
+def interleave(lst1, lst2):
+    alternating_elements = []
+    lst_of_tuples = list(zip(lst1, lst2))
+    for items in lst_of_tuples:
+        alternating_elements.extend(list(items))
+    return alternating_elements
+
+# # Example
+list1 = [1, 2, 3]
+list2 = ['a', 'b', 'c']
+expected = [1, "a", 2, "b", 3, "c"]
+print(interleave(list1, list2) == expected)      # True
