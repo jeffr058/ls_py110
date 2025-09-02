2667050bdfb769fb9f2b16f15b8398a85eaeebc9
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-09-02 16:59:58 -0700

    add my solution

---
+++ lesson_02/assignment_07/practice_problem_03.py
@@ -0,0 +1,11 @@
+# Practice Problem 3
+# Given the following data structure, return a new list with the same structure, but with the values in each sublist ordered in ascending order as strings (that is, the numbers should be treated as strings). Use a comprehension if you can. (Try using a for loop first.)
+
+lst = [['b', 'c', 'a'], [2, 11, -3], ['blue', 'black', 'green']]
+
+# Expected result
+[['a', 'b', 'c'], [-3, 11, 2], ['black', 'blue', 'green']]
+
+# My Answer
+new_list = [sorted(sublist, key=str) for sublist in lst]
+print(new_list)
