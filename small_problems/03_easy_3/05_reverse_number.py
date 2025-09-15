34071d13bd372aebb6a62a89d1d4bcb5b7f5c29a
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-09-15 11:24:55 -0700

    add my solution

---
+++ small_problems/03_easy_3/05_reverse_number.py
@@ -0,0 +1,26 @@
+# inputs: positive integer
+# outputs: integer w/ digits reversed
+# implicit rules: 
+    # single digit returns same single digit
+    # trailing zeros are removed
+# data structure: maybe list
+# algorithm:
+    # Given an integer
+    # Reverse the order of integer
+        # Convert integer to string
+        # Reverse the order of the string
+            # Slicing entire string with negative step
+        # Convert back to integer
+    # Return the result
+# code:
+def reverse_number(integer):
+    string = str(integer)[::-1]
+    result = int(string)
+    
+    return result
+
+print(reverse_number(12345) == 54321)   # True
+print(reverse_number(12213) == 31221)   # True
+print(reverse_number(456) == 654)       # True
+print(reverse_number(1) == 1)           # True
+print(reverse_number(12000) == 21)      # True
