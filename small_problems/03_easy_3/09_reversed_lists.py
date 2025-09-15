dda3153b187bf9781a3c4dfd063edfa5ce8e8981
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-09-15 14:27:51 -0700

    add my solution

---
+++ small_problems/03_easy_3/09_reversed_lists.py
@@ -0,0 +1,43 @@
+# input: list
+# output: list of elements reversed
+# rules: 
+    # mutate the arg
+    # don't reassign input list
+    # don't use reverse method or [::-1]
+    # list w/ single element stays same
+    # empty list stays empty
+# data structure: list
+# algorithm:
+    # Given list1
+    # Mutate list1 by reversing elements
+    # Return list1
+# code:
+def reverse_list(list1):
+    items = list1.copy()
+    list1.clear()
+
+    for idx in range(len(items) - 1, -1, -1):
+        list1.append(items[idx])
+
+    return list1
+
+# Test Cases
+list1 = [1, 2, 3, 4]
+result = reverse_list(list1)
+print(result == [4, 3, 2, 1])               # True
+print(list1 is result)                      # True
+
+list2 = ["a", "b", "c", "d", "e"]
+result2 = reverse_list(list2)
+print(result2 == ['e', 'd', 'c', 'b', 'a']) # True
+print(list2 is result2)                     # True
+
+list3 = ["abc"]
+result3 = reverse_list(list3)
+print(result3 == ['abc'])                   # True
+print(list3 is result3)                     # True
+
+list4 = []
+result4 = reverse_list(list4)
+print(result4 == [])                        # True
+print(list4 is result4)                     # True
