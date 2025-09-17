82da240bee9a522af340ae3d5c5a6a7829b0bf46
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-09-17 15:49:31 -0700

    add my solution

---
+++ small_problems/05_easy_5/08_staggered_case_part_1.py
@@ -0,0 +1,24 @@
+def staggered_case(string):
+    result = []
+
+    for idx, char in enumerate(string):
+        if idx % 2 == 0:
+            result.append(char.upper())
+        else:
+            result.append(char.lower())
+    
+    return ''.join(result)
+
+string = 'I Love Launch School!'
+result = "I LoVe lAuNcH ScHoOl!"
+print(staggered_case(string) == result)  # True
+
+string = 'ALL_CAPS'
+result = "AlL_CaPs"
+print(staggered_case(string) == result)  # True
+
+string = 'ignore 77 the 4444 numbers'
+result = "IgNoRe 77 ThE 4444 nUmBeRs"
+print(staggered_case(string) == result)  # True
+
+print(staggered_case('') == "")          # True
