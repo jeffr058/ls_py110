2811e33e77cdfd0476b6379b52c08b566948d893
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-09-17 10:48:27 -0700

    add my solution

---
+++ small_problems/05_easy_5/01_inverting_dictionary.py
@@ -0,0 +1,29 @@
+# input: dictionary w/ unique keys & values
+# output: dictionary w/ keys & values inverted
+# implicit rule: return the dictionary
+# data structure: dictionary, maybe list?
+# algorithm:
+    # Given a dictionary
+    # Swap the keys and values
+        # Set inverted_items to empty dictionary
+        # Create coll of keys and coll of values
+        # Merge the value at each positon with key into separate coll
+            # Pass colls to zip function and convert return obj to list
+        # Convert object to dictionary and assign to inverted_items
+    # Return inverted_items
+# code:
+def invert_dict(dictionary):
+    zipped_list = list(zip(dictionary.values(), dictionary.keys()))
+    inverted_items = dict(zipped_list)
+    
+    return inverted_items
+
+print(invert_dict({
+          'apple': 'fruit',
+          'broccoli': 'vegetable',
+          'salmon': 'fish',
+      }) == {
+          'fruit': 'apple',
+          'vegetable': 'broccoli',
+          'fish': 'salmon',
+      })  # True
