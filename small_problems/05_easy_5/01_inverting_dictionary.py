2e27263d24d546a50d858d461f14a13c3c53498c
Jeff Ro <44447450+jeffr058@users.noreply.github.com>
2025-09-17 10:55:12 -0700

    add second solution

--- small_problems/05_easy_5/01_inverting_dictionary.py
+++ small_problems/05_easy_5/01_inverting_dictionary.py
@@ -11,12 +11,21 @@
             # Pass colls to zip function and convert return obj to list
         # Convert object to dictionary and assign to inverted_items
     # Return inverted_items
+
+    # given a dictionary
+    # change keys to values and values to keys
+        # set inverted_dict to empty dictionary
+        # for each key and value in dictionary
+            # add the value as key assigned with key
+    # return inverted_dict
 # code:
 def invert_dict(dictionary):
-    zipped_list = list(zip(dictionary.values(), dictionary.keys()))
-    inverted_items = dict(zipped_list)
+    # zipped_list = list(zip(dictionary.values(), dictionary.keys()))
+    # inverted_items = dict(zipped_list)
     
-    return inverted_items
+    # return inverted_items
+
+    return {value: key for key, value in dictionary.items()}
 
 print(invert_dict({
           'apple': 'fruit',
